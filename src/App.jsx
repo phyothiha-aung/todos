2bc86c25444c5c417d80600c3ee6c74bc805632e
phyothiha-aung <phyothiha050@gmail.com>
2023-03-22 16:14:21 +0630

    added react icons

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="flex justify-center items-center w-full h-screen">
-      <div className="bg-gray-300 w-[1000px]">
+      <div className="bg-gray-300 max-w-[700px] w-full">
         <h1 className="text-3xl text-black text-center font-bold m-3">
           Todo List
         </h1>
@@ -13,6 +13,11 @@ function App() {
           name="search"
           placeholder="Search"
         />
+        <div className="w-full">
+          <div className="flex flex-col p-2">
+            <p className="text-lg">Item</p>
+          </div>
+        </div>
       </div>
     </div>
   );
